8ebae653b49240481f7dd10579df09dfb7daae88
pulorsok <pulorsok@gmail.com>
2018-04-27 16:14:29 +0800

    add tmux install sh

---
+++ tmux_install.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+    set -e
+# Show progress
+    set -x
+# Download config from bash_it framework
+    cd ${HOME}
+    git clone https://github.com/gpakosz/.tmux.git
+    ln -s -f .tmux/.tmux.conf
+    cp .tmux/.tmux.conf.local .
